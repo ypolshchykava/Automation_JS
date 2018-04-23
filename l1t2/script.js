8376bb929d13f8f391e19069ea02731381decf38
yulia_usovich@mail.ru <ypolshchykava@exadel.by>
2018-04-23 20:17:55 +0300

    minor changes

--- l1t2/script.js
+++ l1t2/script.js
@@ -1,25 +1,11 @@
-var a = 1;
-var b = 40;
-var c = 1;
-var D;
-var x1;
-var x2;
-var text_no_roots = "I've got no roots, but my home was never on the ground \nI've got no roots, but my home was never on the ground \nI've got no roots uh uh uh uh \nI've got no roots uh uh uh uh…";
-var text_two_roots = "I have 2 roots. My roots are: \n"
-var text_one_root = "I have 1 root. My root is: \n"
+var n = 30;
+var sum = 0;
 
-D = b * b - 4 * a * c;
-if (D != 0) {
-	if (D > 0) {
-		x1 = (-b + D^0.5)/(2*a);
-		x2 = (-b - D^0.5)/(2*a);
-		console.log(text_two_roots+"x1 = "+x1+"\n"+"x2 = "+x2);
+for (var i = 2; i <= n/2; i++) {
+		if (n % i === 0) {
+			sum = sum + i;
+		}
 	}
-	else {
-		console.log(text_no_roots);
-	}
-}
-else {
-	x1 = (-b + D^0.5)/(2*a);
-	console.log(text_one_root+"x="+x1);
-}
+	
+console.log(sum === 0);
+		
