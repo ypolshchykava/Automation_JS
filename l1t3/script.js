782dc7bc33f0bafa6b1c43f355b992901ec9e9d0
yulia_usovich@mail.ru <ypolshchykava@exadel.by>
2018-04-24 13:35:01 +0300

    minor changes

--- l1t3/script.js
+++ l1t3/script.js
@@ -1,9 +1,14 @@
-var n = 3;
+var n = 25;
 var sum = 0;
+var text_invalid_value = "The value you've provided is not a number.\nPlease provide correct value."
 
+if (typeof n === "number") {
 for (var i = 1; i <= n; i++) {
 		sum = sum + 1/i;
-	}
-	
+	}	
 console.log(sum);
-		
+}
+
+else {
+	console.log(text_invalid_value);
+}		
