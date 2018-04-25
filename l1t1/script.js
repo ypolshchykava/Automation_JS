94ae5faeeb48b3e934ca736320cf8945d87d0ef6
yulia_usovich@mail.ru <ypolshchykava@exadel.by>
2018-04-25 19:05:25 +0300

    minor updates

--- l1t1/script.js
+++ l1t1/script.js
@@ -1,6 +1,6 @@
 var a = 1;
 var b = 2;
-var c = 1;
+var c = "kk";
 var D;
 var x1;
 var x2;
@@ -9,7 +9,7 @@ var text_two_roots = "I have 2 roots. My roots are: \n"
 var text_one_root = "I have 1 root. My root is: \n"
 var text_invalid_values = "Not all the values you've provided are numbers.\nPlease provide correct values."
 
-if ((typeof a === "number") & (typeof b === "number") & (typeof c === "number")) {
+if ((typeof a === "number") && (typeof b === "number") && (typeof c === "number")) {
 	D = b*b - 4*a*c;
 	if (D !== 0) {
 		if (D > 0) {
