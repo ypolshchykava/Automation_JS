073585590725735db24b5340441734be37e945f2
ypolshchykava <38661029+ypolshchykava@users.noreply.github.com>
2018-05-06 21:53:40 +0300

    Update functions.js

--- ToDoPaper/functions.js
+++ ToDoPaper/functions.js
@@ -1,59 +1,61 @@
 // addTodoItem(todoItem: object)
-//Check if todoItem is valid and if yes, add it to todoItems array. 
-//Function should return the result of validity check (text field is not empty, all fields are present and id is unique). 
-//todoItem : { text: string, completed: boolean, id: number }. 
- function addTodoItem(todoItem){
-	var ifValid = true;
+//Check if todoItem is valid and if yes, add it to todoItems array.
+//Function should return the result of validity check (text field is not empty, all fields are present and id is unique).
+//todoItem : { text: string, completed: boolean, id: number }.
+function addTodoItem(todoItem){
+    let ifValid = true;
 
-	if(todoItem["text"].length === 0 || (todoItem["completed"]!=true && todoItem["completed"]!=false  )|| todoItem["id"].length === 0 ){
-		ifValid = false;
-		return ifValid;
-	}
-	//check for uniqueness of id
-	else {
-		for (var i in todoItems){
-			if(todoItems[i]["id"] ===todoItem["id"]) {
-				ifValid = false;
-				return ifValid;
 
-			}
-		}
-	}
-	todoItems.push(todoItem);
-	return ifValid;
+    if(todoItem["text"].length === 0 || typeof(todoItem["completed"]) !== typeof(true) || todoItem["id"].length === 0 ){
+        ifValid = false;
+        return ifValid;
+    }
+    //check for uniqueness of id
+    else {
+        for (let i in todoItems){
+            if(todoItems[i]["id"] ===todoItem["id"]) {
+                ifValid = false;
+                return ifValid;
+
+            }
+        }
+    }
+    todoItems.push(todoItem);
+    return ifValid;
 }
 
 
 // viewTodoList(itemsType: string)
-//Function takes itemsType argument (‘completed’, ‘not_completed’, ‘all’) and returns all items of this type. 
+//Function takes itemsType argument (â€˜completedâ€™, â€˜not_completedâ€™, â€˜allâ€™) and returns all items of this type.
 function viewTodoList(typeOfTodoItem) {
 
+    let resultArray;
     if (typeOfTodoItem === "all") {
         return todoItems;
     }
 
     else if(typeOfTodoItem === "completed"){
-    	var resultArray = todoItems.filter(function (item) {
+        resultArray = todoItems.filter(function (item) {
             return item["completed"] === true;
         });
         return resultArray;
     }
     else if(typeOfTodoItem === "not_completed"){
-    	var resultArray = todoItems.filter(function (item) {
+        resultArray = todoItems.filter(function (item) {
             return item["completed"] === false;
         });
-        return resultArray;	
+        return resultArray;
     }
-     
+
 }
 
-// editTodoItem(todoItemId: number, newText: string) 
-//If newText is not empty, function changes text of todoItem by todoItemId on the new text. 
-//It should return flag, whether edit was successful. 
+// editTodoItem(todoItemId: number, newText: string)
+//If newText is not empty, function changes text of todoItem by todoItemId on the new text.
+//It should return flag, whether edit was successful.
 function editTodoItem(number, newText) {
-    var IfUpdated = false;
-    if (newText.length != 0) {
-        for (var i in todoItems) {
+    let IfUpdated = false;
+    if (newText.length !== 0) {
+        for (let i in todoItems) {
             if (todoItems[i]["id"] === number) {
                 todoItems[i]["text"] = newText;
                 IfUpdated = true;
@@ -68,27 +70,27 @@ function editTodoItem(number, newText) {
 }
 
 // deleteTodoItem(todoItemId: number)
-//Delete todoItem by todoItemId, return flag, whether delete was successful. 
-//todoItem : { text: string, completed: boolean, id: number }. 
-function deleteTodoItem(todoItemId){	
-	var ifDeleted = false;
-	for (var i = 0; i < todoItems.length; i++) {
-		if(todoItems[i]['id'] === todoItemId){
-			todoItems.splice(i,1);
-			ifDeleted = true;
-			break;
-		}
-	}
-	return ifDeleted;
+//Delete todoItem by todoItemId, return flag, whether delete was successful.
+//todoItem : { text: string, completed: boolean, id: number }.
+function deleteTodoItem(todoItemId){
+    let ifDeleted = false;
+    for (let i = 0; i < todoItems.length; i++) {
+        if(todoItems[i]['id'] === todoItemId){
+            todoItems.splice(i,1);
+            ifDeleted = true;
+            break;
+        }
+    }
+    return ifDeleted;
 }
 
-// completeTodoItem(todoItemId: number) 
+// completeTodoItem(todoItemId: number)
 //Change completed field of todoItem (get it by todoItemId) on true.
-function completeTodoItem(todoItemId){	
-	for (var i = 0; i < todoItems.length; i++) {
-		if(todoItems[i]["id"]===todoItemId){
-			todoItems[i]["completed"] = true;
-			break;
-		}
-	}
+function completeTodoItem(todoItemId){
+    for (let i = 0; i < todoItems.length; i++) {
+        if(todoItems[i]["id"]===todoItemId){
+            todoItems[i]["completed"] = true;
+            break;
+        }
+    }
 }
