516b80dd73edf8f6f6e5067ea0df086c2d7262e8
ypolshchykava <38661029+ypolshchykava@users.noreply.github.com>
2018-05-06 20:46:27 +0300

    Update script.js

--- l3t1/script.js
+++ l3t1/script.js
@@ -1,1 +1,41 @@
+function Machine(power) {
+    this._power = power;
+    this._enabled = false;
+    var self = this;
+    this.enable = function () {
+        self._enabled = true;
+    };
+    this.disable = function () {
+        self._enabled = false;
+    };
+}
 
+function Fridge(power) {
+    Machine.apply(this, arguments);
+    var food = [];
+    var maxQuantity = power / 100;
+
+    this.addFood = function (item) {
+        if (!this._enabled) {
+            console.log("The fridge is off. Turn it on to add food.");
+            return;
+        }
+
+        let newfood = item.split(', ');
+        if ((newfood.length + food.length) > maxQuantity) {
+            console.log("The fridge is full. There is not enough free space.");
+        }
+        else {
+            for (var i = 0; i < newfood.length; i++) {
+                food.push(newfood[i]);
+
+            }
+        }
+        console.log(food);
+
+    }
+}
+
+var myFridgeTest = new Fridge(100);
+myFridgeTest.enable();
+myFridgeTest.addFood("lemon, apple, apple, melon, pancake");
