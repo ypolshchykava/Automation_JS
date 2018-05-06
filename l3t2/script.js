df451d2181afb6079d3c3fb33051354699c62bd1
ypolshchykava <38661029+ypolshchykava@users.noreply.github.com>
2018-05-06 21:06:49 +0300

    Update script.js

--- l3t2/script.js
+++ l3t2/script.js
@@ -1,1 +1,12 @@
+let initial = [1, 2, 1, -2, 3.14, 4, -2, 1, -10, 5, -5];
 
+let positive = initial.filter(function(number){
+    return number>0;
+});
+
+let negative = initial.filter(function(number){
+    return number<0;
+});
+
+console.log(positive);
+console.log(negative);
