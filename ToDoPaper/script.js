4f2c6a121903926ff4fd25adf0b486abeb3d3ab2
ypolshchykava <38661029+ypolshchykava@users.noreply.github.com>
2018-05-06 22:16:16 +0300

    Update script.js

--- ToDoPaper/script.js
+++ ToDoPaper/script.js
@@ -1,1 +1,9 @@
+let toDoItems = [];
 
+let loadToDoItems = function () {
+    $.getJSON("todos.json", function (data) {
+        console.log(data);
+        toDoItems = data;
+    });
+};
+loadToDoItems();
