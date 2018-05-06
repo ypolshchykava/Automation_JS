3712e97cc2b056538ef36afc798db9712c53060b
ypolshchykava <38661029+ypolshchykava@users.noreply.github.com>
2018-05-06 21:16:10 +0300

    Update script.js

--- l3t2/script.js
+++ l3t2/script.js
@@ -8,5 +8,16 @@ let negative = initial.filter(function(number){
     return number<0;
 });
 
+let distinct = {};
+initial.forEach(function(number){
+    if(distinct[number]===undefined) {
+        distinct[number] =  1;
+    }
+    else {
+        distinct[number]  += 1;
+    }
+});
+
 console.log(positive);
 console.log(negative);
+console.log(distinct);
