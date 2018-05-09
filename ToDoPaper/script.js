1d20aca7b0616c10709a7c6b7fcc5fd0fac89712
yulia_usovich@mail.ru <ypolshchykava@exadel.by>
2018-05-09 12:54:25 +0300

    homework

--- ToDoPaper/script.js
+++ ToDoPaper/script.js
@@ -1,9 +1,19 @@
-let toDoItems = [];
+let todoItems = [];
 
 let loadToDoItems = function () {
     $.getJSON("todos.json", function (data) {
         console.log(data);
-        toDoItems = data;
-    });
+        todoItems = data;
+    }).done(function(){
+		
+			
+			viewTodoListDom("all");			
+			// editTodoItemDom(55,"fsdfs");  // negative
+			addTodoItemDom({ "text": " NEW ITEM ADDED", "completed": true, "id": 3 });			
+			// deleteTodoItemDom(22);  // negative
+			
+
+		});
 };
+
 loadToDoItems();
