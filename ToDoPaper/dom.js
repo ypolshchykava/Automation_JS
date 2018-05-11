b641bff3a57a897634c246f45f37a68eacc4fc75
yulia_usovich@mail.ru <ypolshchykava@exadel.by>
2018-05-11 20:34:26 +0300

    event handlers added

--- ToDoPaper/dom.js
+++ ToDoPaper/dom.js
@@ -17,6 +17,8 @@ let todoList = viewTodoList(itemsType) ;
 
  function addTodoItemDom(todoItem){
  	if(addTodoItem(todoItem)){
+ 		// extract field from radiobbutton dynamiccally
+ 		// document.getElementById('viewType').getElementsByTagName('input')
  		viewTodoListDom("all");		
  	}
  	else {
@@ -31,25 +33,26 @@ function addTodoToHtml(toDoItem){
 		div.id = toDoItem.id;
 	let todoHtml;	
 	let checkBoxAttr = toDoItem.completed===true ? "checked" : "";
-	let isCompleted = toDoItem.completed===true ? "green" : "red";
-todoHtml = `<div class="w3-container">
-	<button type="button" class="btn btn-info" data-toggle="collapse" style="width:600px;height:40px;" data-target="#demo${toDoItem.id}">To do item #${toDoItem.id} </button>
+	let isCompleted = toDoItem.completed===true ? "disabled" : "enabled";
+	let isCompletedcol = toDoItem.completed===true ? "#4CAF50" : "#e7e7e7";
+	todoHtml = `<div class="w3-container">
+	<button type="button" class="btn btn-info" data-toggle="collapse" style="width:600px;height:40px;background-color = ${isCompletedcol};" data-target="#demo${toDoItem.id}">To do item #${toDoItem.id} </button>
 	<div id="demo${toDoItem.id}" class="collapse">
 	</p>
 	<b>Text:</b>
 	</p>
-    <textarea rows="4" cols="50">${toDoItem.text}</textarea>	
-    <div class="checkbox">
+    <textarea rows="4" cols="50" ${isCompleted}>${toDoItem.text}</textarea>	
+    <div class="checkbox" ${isCompleted}>
   	<label><input type="checkbox" value="" ${checkBoxAttr}>Is Completed</label>
-  	<div class="w3-panel w3-${isCompleted}" style="width:120px;height:20px;"> ${toDoItem.completed}</div>
-	</div>
+  	</div>
+  	<button type="button" id="saveToDo" class="btn btn-info" ${isCompleted} style="width:70px;height:30px;background-color: #4CAF50;">Save</button>
+  	<button type="button" id="deleteToDo" class="btn btn-info" ${isCompleted} style="width:70px;height:30px;background-color: #f44336;">Delete</button>
 	</div></div></br></br>`
 	
 	div.innerHTML = todoHtml;
 	return div;
 }
   
-
 
    // editTodoItemDom(todoItemId: number, newText: string) 
    // It should replace the text of the item with the given todoItemId. 
@@ -69,7 +72,12 @@ todoHtml = `<div class="w3-container">
 
 function deleteTodoItemDom(todoItemId){
 	if(deleteTodoItem(todoItemId)){
-		viewTodoListDom("all");	
+		// viewTodoListDom("all");	
+		const list = document.getElementById('todo-items');
+		// list.removeChild(event.target.parentElement.parentElement.parentElement);
+		// list.removeChild(todoItemId);
+		list.removeChild(document.getElementById(todoItemId));
+
 	}
 	else{
    		alert("Unable to delete a Todo Item with given id"+todoItemId);
